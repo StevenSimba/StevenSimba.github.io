fd3e48e3d7a006072b328480a9242e04beeb05c5
stevensimba <stir2017@gmail.com>
2020-04-12 22:04:50 +0300

    adding right margin to the cover image

---
+++ styles-7b7fc8ed780923f65fbf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{189:function(n,o,c){},211:function(n,o,c){},212:function(n,o,c){},213:function(n,o,c){}}]);
+//# sourceMappingURL=styles-7b7fc8ed780923f65fbf.js.map
